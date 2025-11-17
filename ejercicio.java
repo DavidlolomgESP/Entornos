ba51a8cee5bc0968c066be605a73cf918845e99b
David <davidlolomg123@gmail.com>
2025-11-17 08:50:31 +0100

    Primer Ejercicio v2

--- ejercicio.java
+++ ejercicio.java
@@ -1,6 +1,11 @@
 public class Ejercicio1(
     public static void main(String[] args){
         System.out.println("Hola mundo");
+
+        int n,m;
+        n=5;
+        m=3;
+        System.out.println(n + m)
     }
 
 
