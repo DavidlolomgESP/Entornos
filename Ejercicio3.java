6ebb0f2f93e476d845988d91e19f4ffb7d6a36a4
DavidlolomgESP <davidlolomg123@gmail.com>
2025-11-17 09:00:55 +0100

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,12 @@
+public class Ejercicio1(
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+
+        int n,m;
+        n=5;
+        m=3;
+        System.out.println("Division = " + n / m)
+    }
+
+
+)
